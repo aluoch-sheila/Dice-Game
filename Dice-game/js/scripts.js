b3ecba915454f7b65f2c0c86fb8ad4be474797d4
aluoch-sheila <aluochsheila1999@gmail.com>
2018-09-29 14:05:56 +0300

    Added about2.html

--- Dice-game/js/scripts.js
+++ Dice-game/js/scripts.js
@@ -1,14 +1,90 @@
- function moveDie(){
-   var dieOne=document.getElementById("dieOne")
-   var dieTwo=document.getElementById("dieTwo")
-   // var show
-   var D1=Math.floor(Math.random()*6)+1;
-   var D2=Math.floor(Math.random()*6)+1;
-   var dieTotal=D1+D2;
-   dieOne.innerHTML=D1;
-   dieTwo.innerHTML=D2;
-   show.innerHTML="You moved " + dieTotal +".";
-   if( D1 == D2 ){
-      show.innerHTML+= " SAME SCORE!,a free turn for you guys!!"
-   }
- }
+
+function Player(name, spin, score) {
+  this.name = name;
+  this.spin = spin;
+  this.score = score;
+}
+
+Player.prototype.spining = function() {
+ var playerSpin = Math.floor((Math.random() * 6) + 1);
+ $(".currentSpin").text(playerSpin);
+    if (playerSpin === 1) {
+      alert("You got a '1'! Next Player!");
+      return this.spin = 0;
+    } else {
+      this.spin = (this.spin + playerSpin);
+      return this.spin;
+    }
+}
+
+Player.prototype.holding = function() {
+
+  this.score = (this.spin + this.score);
+  this.spin = 0;
+  if(this.score >= 100){
+    confirm("Congratulations! ");
+    if (true) {
+      location.reload();
+    }
+  }
+  return this.score;
+
+}
+
+
+
+
+$(document).ready(function(){
+  $("#firstPlayer").submit(function(event){
+    event.preventDefault();
+
+    var pOneInput = $("#firstPlayer-name").val();
+    onePlayer = new Player(pOneInput, 0, 0);
+    $(".firstPlayerName").text(onePlayer.name);
+    $(".playOneDiv").hide();
+  });
+
+  $("#secondPlayer").submit(function(event){
+    event.preventDefault();
+
+    var pTwoInput = $("#secondPlayer-name").val();
+    twoPlayer = new Player(pTwoInput, 0, 0);
+    $(".secondPlayerName").text(twoPlayer.name);
+    $(".playTwoDiv").hide();
+
+  });
+
+  $("#p1button").click(function(event){
+    event.preventDefault();
+
+    var oneSpin = onePlayer.spining();
+    $(".p1spin").text(oneSpin);
+
+  });
+  $("#p2button").click(function(event){
+    event.preventDefault();
+
+    var twoSpin = twoPlayer.spining();
+    $(".p2spin").text(twoSpin);
+
+   });
+
+
+
+  $(".p1hold").click(function(event){
+    event.preventDefault();
+
+    var oneScore = onePlayer.holding();
+    $(".p1score").text(oneScore);
+    $(".p1spin").text("");
+
+  });
+  $(".p2hold").click(function(event){
+    event.preventDefault();
+
+    var twoScore = twoPlayer.holding();
+    $(".p2score").text(twoScore);
+    $(".p2spin").text("");
+  });
+
+});
